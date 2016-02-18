f810b6cb43d53287587ca57e602511bc5376ba8d
Ezequiel Urrizaga <ezequiel.urrizaga@globant.com>
2016-02-18 15:49:22 -0300

    - jasmine

--- app/register/register_test.js
+++ app/register/register_test.js
@@ -1,32 +1,16 @@
 'use strict';
 
 describe('myApp.register module', function() {
-  var scope, $location, $controller, appData, services;
-  var createController;
-  
-  
+
   beforeEach(module('myApp.register'));
-  
-
-  beforeEach(inject(function ($rootScope, $controller, _$location_) {
-    
-    $location = _$location_;
-    scope = $rootScope.$new();
-
-    createController = function() {
-      return $controller('RegisterCtrl', {
-        '$scope': scope,
-        'services': services
-      });
-    };
-  }));
 
   describe('register controller', function(){
 
-    it('should ....', function($controller) {
-      var controller = createController();
-    });
+    it('should ....', inject(function($controller) {
+      //spec body
+      var RegisterCtrl = $controller('RegisterCtrl');
+      expect(RegisterCtrl).toBeDefined();
+    }));
 
   });
-
 });
